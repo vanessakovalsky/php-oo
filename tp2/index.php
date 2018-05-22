398714cd7c6e5e3283a0f782e2c9687afeca4523
Vanessa DAVID <vanessa.david@alten.com>
2018-05-22 14:39:00 +0200

    Ajout template de base

--- tp2/index.php
+++ tp2/index.php
@@ -1,6 +1,8 @@
 <?php
-
+include_once('src/View/header.html.php');
 include_once('src/Controller/JeuController.class.php');
 
 $jeu = new Jeu();
 $jeu->AjoutJeu();
+
+include_once('src/View/footer.html.php');
