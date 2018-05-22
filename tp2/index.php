9e82163a17bcc65a134f4175de57d3983ee62927
Vanessa DAVID <vanessa.david@alten.com>
2018-05-22 14:24:00 +0200

    Ajout TP2

---
+++ tp2/index.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once('src/Controller/JeuController.class.php');
+
+$jeu = new Jeu();
+$jeu->AjoutJeu();
---
+++ tp2/src/Controller/JeuController.class.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *
+ */
+class Jeu
+{
+  private $nom_jeu;
+  private $editeur;
+
+  function __construct()
+  {
+    // code...
+  }
+
+  public function AjoutJeu(){
+    // Enregistrement des données en base de données
+    echo 'Mon jeu a été ajouté !';
+  }
+
+  public function SuppressionJeu(){
+
+  }
+
+  public function ModificationJeu(){
+
+  }
+
+}
+ ?>
