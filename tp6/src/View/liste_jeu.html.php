a5272710657091cf769cf2b878e28a23c7401799
Vanessa DAVID <vanessa.david@alten.com>
2018-05-23 17:16:53 +0200

    Affichage liste en mode grille

--- tp6/src/View/liste_jeu.html.php
+++ tp6/src/View/liste_jeu.html.php
@@ -1,7 +1,20 @@
 <?php
-
-foreach($reponse_voir_jeu as $jeu){
-  include('./src/View/voir_jeu.html.php');
-  ?><hr>
+$chunk_reponse = array_chunk($reponse_voir_jeu, 4);
+foreach($chunk_reponse as $array_jeu){
+    ?>
+    <div class="row">
   <?php
-}
+  foreach($array_jeu as $jeu ){
+  ?>
+  <div class="col-sm-3">
+  <?php
+  include('./src/View/voir_jeu.html.php');
+  ?>
+  </div>
+  <?php
+ }
+    ?>
+  </div>
+  <hr>
+    <?php
+  }
