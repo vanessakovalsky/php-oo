4d8d456502290d9a2696b29c646e5a47f1fb9171
vanessakovalsky <v.david@kovalibre.com>
2024-01-05 16:07:13 +0100

    Update Autoloader.class.php

--- tp9/src/Autoloader.class.php
+++ tp9/src/Autoloader.class.php
@@ -1,13 +1,16 @@
 <?php
 
 class Autoloader {
-  static function register(){
-    spl_autoload_register(array(__CLASS__, 'autoload'));
-  }
+    static function register(){
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
 
-  static function autoload($class){
-    require(__DIR__.'\\'.$class.'.class.php');
-  }
+    static function autoload($class){
+        $class_path = str_replace('\\', DIRECTORY_SEPARATOR, $class); 
+        $file =  __DIR__ .'/'. $class_path . '.class.php';
+        
+        require_once($file);
+    }
 }
 
  ?>
